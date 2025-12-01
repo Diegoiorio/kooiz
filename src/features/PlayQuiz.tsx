4e05f329be9f8c5917ede35a1e9cd3a4192eede6
Diego <diegoiorio.di@gmail.com>
2025-12-01 22:32:38 +0100

    Refactoring pt2

--- src/features/PlayQuiz.tsx
+++ src/features/PlayQuiz.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import type { DotLottiePlayer, QuizItem } from "../types/quis-types";
 import { Box, Flex, Heading, RadioGroup, SimpleGrid } from "@chakra-ui/react";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
@@ -6,93 +6,32 @@ import validAnim from "../assets/valid.json?url";
 import invalidAnim from "../assets/invalid.json?url";
 import { Timer } from "./Timer";
 import { ProgressBar } from "../components/ProgressBar";
+import { usePlayQuizState } from "../hooks/usePlayQuizState";
 
 export function PlayQuiz(p: {
   quiz: QuizItem[];
   onFinished: (history: boolean[]) => void;
 }) {
-  const questionSecTimer = 20;
-
-  const [currentQuizItemIndex, setCurrentQuizItemIndex] = useState<number>(0);
-
-  const [history, setHistory] = useState<boolean[]>([]);
-
-  // Available answers handler
-  const [availableAnswers, setAvailableAnswers] = useState<string[]>([]);
-
-  const [answer, setAnswer] = useState<string | null>(null);
-
-  const [questionStatus, setQuestionStatus] = useState<
-    "valid" | "invalid" | "unanswered"
-  >("unanswered");
-
-  // Dot Lottie handler
-  const [dotLottie, setDotLottie] = useState<DotLottiePlayer | null>(null);
-
-  const currentQuizItem = p.quiz[currentQuizItemIndex];
-
-  useEffect(() => {
-    setAvailableAnswers(
-      [
-        currentQuizItem.correct_answer,
-        ...currentQuizItem.incorrect_answers,
-      ].sort(() => Math.random() - 0.5) // Shuffle answers
-    );
-  }, [
-    currentQuizItem.correct_answer,
-    currentQuizItem.incorrect_answers,
+  const {
+    questionSecTimer,
     currentQuizItemIndex,
-  ]);
-
-  const isValidAnswer = (answer: string): boolean => {
-    return answer === currentQuizItem.correct_answer;
-  };
-
-  useEffect(() => {
-    if (answer) {
-      const isValid = isValidAnswer(answer);
-      if (isValid) {
-        setQuestionStatus("valid");
-      } else {
-        setQuestionStatus("invalid");
-      }
-      setHistory([...history, isValid]);
-    }
-  }, [answer]);
+    history,
+    setHistory,
+    availableAnswers,
+    answer,
+    setAnswer,
+    questionStatus,
+    setQuestionStatus,
+    setDotLottie,
+    isValidAnswer,
+    currentQuizItem,
+  } = usePlayQuizState(p.quiz, p.onFinished);
 
   const decodeHtmlEntities = (str: string) => {
     const txt = document.createElement("textarea");
     txt.innerHTML = str;
     return txt.value;
   };
-
-  useEffect(() => {
-    if (!dotLottie) return;
-
-    // This function will be called when the animation is completed.
-    function onComplete() {
-      if (currentQuizItemIndex < p.quiz.length - 1) {
-        setQuestionStatus("unanswered");
-        setCurrentQuizItemIndex(currentQuizItemIndex + 1);
-      } else {
-        p.onFinished(history);
-      }
-    }
-
-    // This function will be called when the animation starts playing.
-    function onPlay() {
-      // Could be useful for future implementations
-    }
-
-    // Add event listener on dotLottie component
-    dotLottie.addEventListener("complete", onComplete);
-    dotLottie.addEventListener("play", onPlay);
-
-    return () => {
-      dotLottie.removeEventListener("complete", onComplete);
-      dotLottie.removeEventListener("play", onPlay);
-    };
-  }, [dotLottie]);
 
   const dotLottieRefCallback = (dotLottie: DotLottiePlayer | null) => {
     setDotLottie(dotLottie);
--- src/hooks/usePlayQuizState.tsx
+++ src/hooks/usePlayQuizState.tsx
@@ -1,4 +1,10 @@
-export function usePlayQuizState() {
+import { useEffect, useState } from "react";
+import type { DotLottiePlayer, QuizItem } from "../types/quis-types";
+
+export function usePlayQuizState(
+  quiz: QuizItem[],
+  onFinished: (history: boolean[]) => void
+) {
   // Quiz timer in seconds for each question
   const questionSecTimer = 20;
 
@@ -6,7 +12,6 @@ export function usePlayQuizState() {
 
   const [history, setHistory] = useState<boolean[]>([]);
 
-  // Available answers handler
   const [availableAnswers, setAvailableAnswers] = useState<string[]>([]);
 
   const [answer, setAnswer] = useState<string | null>(null);
@@ -14,4 +19,80 @@ export function usePlayQuizState() {
   const [questionStatus, setQuestionStatus] = useState<
     "valid" | "invalid" | "unanswered"
   >("unanswered");
+
+  const [dotLottie, setDotLottie] = useState<DotLottiePlayer | null>(null);
+
+  const currentQuizItem = quiz[currentQuizItemIndex];
+
+  useEffect(() => {
+    setAvailableAnswers(
+      [
+        currentQuizItem.correct_answer,
+        ...currentQuizItem.incorrect_answers,
+      ].sort(() => Math.random() - 0.5) // Shuffle answers
+    );
+  }, [
+    currentQuizItem.correct_answer,
+    currentQuizItem.incorrect_answers,
+    currentQuizItemIndex,
+  ]);
+
+  const isValidAnswer = (answer: string): boolean => {
+    return answer === currentQuizItem.correct_answer;
+  };
+
+  useEffect(() => {
+    if (answer) {
+      const isValid = isValidAnswer(answer);
+      if (isValid) {
+        setQuestionStatus("valid");
+      } else {
+        setQuestionStatus("invalid");
+      }
+      setHistory([...history, isValid]);
+    }
+  }, [answer]);
+
+  useEffect(() => {
+    if (!dotLottie) return;
+
+    // This function will be called when the animation is completed.
+    function onComplete() {
+      if (currentQuizItemIndex < quiz.length - 1) {
+        setQuestionStatus("unanswered");
+        setCurrentQuizItemIndex(currentQuizItemIndex + 1);
+      } else {
+        onFinished(history);
+      }
+    }
+
+    // This function will be called when the animation starts playing.
+    function onPlay() {
+      // Could be useful for future implementations
+    }
+
+    // Add event listener on dotLottie component
+    dotLottie.addEventListener("complete", onComplete);
+    dotLottie.addEventListener("play", onPlay);
+
+    return () => {
+      dotLottie.removeEventListener("complete", onComplete);
+      dotLottie.removeEventListener("play", onPlay);
+    };
+  }, [dotLottie]);
+
+  return {
+    questionSecTimer,
+    currentQuizItemIndex,
+    history,
+    setHistory,
+    availableAnswers,
+    answer,
+    setAnswer,
+    questionStatus,
+    setQuestionStatus,
+    setDotLottie,
+    isValidAnswer,
+    currentQuizItem,
+  };
 }
